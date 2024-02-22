d30f362130e82e7f21e0049189c81cf4e2422893
Mindslayer001 <21kd1a1514@lendi.org>
2024-02-22 08:48:41 +0530

    styled Button Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,85 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import "./App.css"
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-    const [url, setUrl] = useState('');
-    const [result, setResult] = useState('');
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(false);
+  const [url, setUrl] = useState("");
+  const [result, setResult] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const handleUrlChange = (e) => {
-        setUrl(e.target.value);
-        setResult('');
-        setError('');
-    };
+  const handleUrlChange = (e) => {
+    setUrl(e.target.value);
+    setResult("");
+    setError("");
+  };
 
-    const fetchData = async () => {
-        try {
-            const regex = /\/reel\/([^\?\/]+)/;
-            const match = url.match(regex);
+  const fetchData = async () => {
+    try {
+      const regex = /\/reel\/([^\?\/]+)/;
+      const match = url.match(regex);
 
-            // Check if there's a match and extract the code
-            const reelCode = match ? match[1] : null;
-            console.log("http://localhost:3001/scrape", { reelCode })
-            setLoading(true);
-            const response = await axios.post('http://localhost:3001/scrape', { reelCode });
-            setResult(response.data.videoData.src); // Updated line
-            setError('');
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            setError('Error fetching data. Please try again.');
-        } finally {
-            setLoading(false);
-        }
-    };
+      // Check if there's a match and extract the code
+      const reelCode = match ? match[1] : null;
+      console.log("http://localhost:3001/scrape", { reelCode });
+      setLoading(true);
+      const response = await axios.post("http://localhost:3001/scrape", {
+        reelCode,
+      });
+      setResult(response.data.videoData.src); // Updated line
+      setError("");
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError("Error fetching data. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    const handleRedirect = () => {
-        // Change the URL to the desired website
-        window.open(result, '_blank');
-    };
+  const handleRedirect = () => {
+    // Change the URL to the desired website
+    window.open(result, "_blank");
+  };
 
+  return (
+    <>
+      <div className="page">
+        <div>
+          <h1 className="title">Instagram Reel Downloader</h1>
 
+          <div className="search-bar">
+            <input
+              type="search"
+              name="search"
+              placeholder="Enter Instagram reel Link"
+              value={url}
+              onChange={handleUrlChange}
+              required
+            />
+            <button
+              className="search-btn"
+              type="submit"
+              onClick={fetchData}
+              disabled={loading}
+            >
+              <span>Scrape Data</span>
+            </button>
+          </div>
 
-    return (
-        <div className='page'>
+          {loading && <div>Loading...</div>}
+          {error && <div>Error: {error}</div>}
+          {result && (
             <div>
-                <h1 className='title'>Instagram Reel Downloader</h1>
-
-                <div class="search-bar">
-                    <input type="search" name="search"
-                        placeholder="Enter Instagram reel Link"
-                        value={url}
-                        onChange={handleUrlChange} required />
-                    <button class="search-btn" type="submit" onClick={fetchData} disabled={loading}>
-                        <span>Scrape Data</span>
-                    </button>
-                </div>
-
-                {loading && <div>Loading...</div>}
-                {error && <div>Error: {error}</div>}
-                {result && <div><button onClick={handleRedirect} className='Btn-download'>Download</button></div>}
+              <button onClick={handleRedirect} className="Btn-download">
+                Download
+              </button>
             </div>
+          )}
         </div>
-    );
+      </div>
+    </>
+  );
 }
 
 export default App;
